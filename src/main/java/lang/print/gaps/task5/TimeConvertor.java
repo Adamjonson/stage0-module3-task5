f5f155d65422a6e64b63c69f1329941291c24f4d
Hp <adkhamjon.abd@gmail.com>
2023-04-09 07:39:04 +0200

    Corrects mistakes

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
-        int minute = 65;
-        int seconds = minute * 60;
+        minutes = 65;
+        float seconds = minutes * 60;
     }
 }
